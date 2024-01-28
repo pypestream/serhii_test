34a69dec839d9e409c6afecbdb593d0f91f440fe
Serhii Chupryna <schupryna@pypestream.com>
2024-01-28 12:10:04 +0100

    fixes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+// import * as core from "@actions/core";
+// import * as github from "@actions/github";
+const core = require("@actions/core");
+const github = require("@actions/github");
 // import type { ReleaseReleasedEvent } from "@octokit/webhooks-types";
 // import {sendReleaseNotification} from './send-release-notification'
 
