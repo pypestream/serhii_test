93d373588ad072cc44c5f71be63c83e458fd9ae7
Serhii Chupryna <schupryna@pypestream.com>
2024-02-01 17:03:25 +0100

    new script

---
+++ scripts/index.js
@@ -0,0 +1,99 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+// const artifact = require("@actions/artifact");
+const { replaceMdImages } = require("./replace-md-images");
+// const { notify } = require("./notify");
+
+// const uploadArtifact = async (infos) => {
+//   const artifactClient = artifact.create();
+//   const artifactName = "md";
+//   const files = infos.map((info) => info.newFileName);
+//   console.log(`artifact: ${JSON.stringify(files)}`);
+//   const rootDirectory = process.cwd();
+//   const options = {
+//     continueOnError: false,
+//   };
+
+//   const uploadResult = await artifactClient.uploadArtifact(
+//     artifactName,
+//     files,
+//     rootDirectory,
+//     options
+//   );
+//   return uploadResult;
+// };
+
+const run = async () => {
+  //   const token = core.getInput("token"); // githubtoken
+  const token = process.env.NPM_TOKEN;
+  //   const secretId = core.getInput("secretId");
+  //   const secretKey = core.getInput("secretKey");
+  //   const bucket = core.getInput("bucket");
+  //   const region = core.getInput("region");
+  //   const prefix = core.getInput("prefix");
+  //   const mdfile = core.getInput("mdfile");
+  //   const webhookUrl = core.getInput("webhookUrl");
+  const webhookUrl = process.env.SLACK_PRERELEASE_WEBHOOK_URL;
+
+  const octokit = new github.getOctokit(token);
+
+  try {
+    const repo = github.context.repo;
+    console.log(`repo: ${JSON.stringify(repo)}`);
+    console.log(`sha: ${github.context.sha}`);
+    const res = await octokit.request(
+      "GET /repos/{owner}/{repo}/commits/{ref}",
+      {
+        owner: repo.owner,
+        repo: repo.repo,
+        ref: github.context.ref,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    let mdfiles = res.data.files
+      .filter(
+        (file) => file.status !== "removed" && file.filename.endsWith(".md")
+      )
+      .map((file) => file.filename);
+    console.log(`markdown files: ${JSON.stringify(mdfiles, undefined, 2)}`);
+
+    // const cosOptions = {
+    //   secretId,
+    //   secretKey,
+    //   bucket,
+    //   region,
+    //   prefix,
+    // };
+    const infos = await Promise.all(
+      mdfiles.map(async (mdfile) => {
+        const { newFileName, previewUrl, newContent } = await replaceMdImages(
+          mdfile
+          //   cosOptions
+        );
+        return {
+          filename: mdfile,
+          newFileName: newFileName,
+          previewUrl,
+          newContent,
+        };
+      })
+    );
+    console.log(
+      `previewUrl: ${JSON.stringify(infos.map((info) => info.previewUrl))}`
+    );
+    // if (infos.length > 0) {
+    //   const result = uploadArtifact(infos);
+    //   console.log(`result: ${JSON.stringify(result)}`);
+    //   if (webhookUrl) {
+    //     notify(infos, webhookUrl);
+    //   }
+    // }
+    // core.setOutput("result", infos);
+  } catch (error) {
+    core.setFailed(`run error: ${error.message}`);
+  }
+};
+
+run();
---
+++ scripts/replace-md-images.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const getMarkdownUrls = require("gh-md-urls");
+// const fetch = require("node-fetch");
+// const { upload, initClient } = require("./cos");
+
+const readFile = (path) => fs.readFileSync(path, "utf8");
+
+const getImages = (content) => {
+  var urls = getMarkdownUrls(content, {
+    repository: "https://github.com/mattdesl/gh-md-urls",
+  });
+
+  const images = urls.filter((item) => item.type === "image");
+  return images;
+};
+
+// const uploadImagesToCos = async (images, cosOptions = {}) => {
+// //   initClient(cosOptions);
+//   const replaceInfos = await Promise.all(
+//     images.map(async (item) => {
+//       const res = await fetch(item.url);
+//       const fileName = item.url.split("/").reverse()[0];
+//       if (!fileName) throw new Error("Error: " + item.url);
+//       if (res.headers.get("content-type").startsWith("image/")) {
+//         const buffer = await res.arrayBuffer();
+//         // const url = await upload(
+//         //   { buffer: Buffer.from(buffer), fileName },
+//         //   cosOptions
+//         // );
+//         return { oldVal: item.url, newVal: url };
+//       }
+//     })
+//   );
+//   return replaceInfos;
+// };
+
+const replaceMarkdownImageUrls = (content, replaceInfos) => {
+  replaceInfos.forEach(({ oldVal, newVal }) => {
+    content = content.replaceAll(oldVal, newVal);
+  });
+  return content;
+};
+
+const replaceMdImages = async (fileName, cosOptions = {}) => {
+  const content = readFile(fileName);
+  const images = getImages(content);
+  console.log("IMAGES: ", JSON.stringify(images, undefined, 2));
+  //   const replaceInfos = await uploadImagesToCos(images, cosOptions);
+  console.log("REPLACE_INFOS: ", JSON.stringify(replaceInfos, undefined, 2));
+  const newContent = replaceMarkdownImageUrls(content, replaceInfos);
+  const newFileName =
+    fileName.substring(0, fileName.length - 3) + "_replaced.md";
+  fs.writeFileSync(newFileName, newContent, "utf-8");
+  // 上传cos
+  //   const previewUrl = await upload(
+  //     { buffer: Buffer.from(newContent, "utf-8"), fileName: newFileName },
+  //     cosOptions
+  //   );
+  return {
+    newFileName,
+    newContent,
+    // previewUrl,
+  };
+};
+
+module.exports = {
+  replaceMdImages,
+};
