6893ea8477755f8101fbcfdeda34932ad930eafe
Serhii Chupryna <schupryna@pypestream.com>
2024-02-01 14:54:52 +0100

    testing geting assets list

--- scripts/release.js
+++ scripts/release.js
@@ -1,19 +1,22 @@
 // https://github.com/tryfabric/notify-slack-on-release/blob/main/src/send-release-notification.ts
-// import * as core from "@actions/core";
-// import * as github from "@actions/github";
 const core = require("@actions/core");
 const github = require("@actions/github");
-// import type { ReleaseReleasedEvent } from "@octokit/webhooks-types";
-const sendReleaseNotification =
-  require("./slack-notification").sendReleaseNotification;
-// const { Octokit } = require("@octokit/core");
+const { markdownToBlocks } = require("@instantish/mack");
+const { v2 } = require("cloudinary");
+
+const cloudinary = v2;
+
+cloudinary.config({
+  cloud_name: "dvnmehqko",
+  api_key: "473343293529714",
+  api_secret: "bE-_OiM7f8DbVP9adlVgiWQx97Q",
+});
 
 const preReleaseWebhook = process.env.SLACK_PRERELEASE_WEBHOOK_URL;
 const releaseDevWebhook = process.env.SLACK_RELEASE_DEV_WEBHOOK_URL;
 const releaseWebhook = process.env.SLACK_RELEASE_WEBHOOK_URL;
 const npmToken = process.env.NPM_TOKEN;
 
-// async function run(): Promise<void> {
 async function run() {
   try {
     console.log(`Sending notification...`);
@@ -30,39 +33,79 @@ async function run() {
       core.setFailed("Action should only be run on release publish events");
     }
 
-    // const octokit = new Octokit({
-    //   auth: npmToken,
-    // });
-
-    // const data = await octokit.request(
-    //   `GET /repos/${repo.owner}/${repo.repo}/releases/${release.id}/assets`,
-    //   {
-    //     owner: repo.owner,
-    //     repo: repo.repo,
-    //     release_id: release.id,
-    //     headers: {
-    //       "X-GitHub-Api-Version": "2022-11-28",
-    //     },
-    //   }
-    // );
-
-    // console.log("RELEASE:", JSON.stringify(release, null, 2));
-
-    // console.log("assets list: ", JSON.stringify(data, null, 2));
-
     const isPreRelease = release.prerelease;
-    // const webhooks = isPreRelease
-    //   ? [preReleaseWebhook]
-    //   : [releaseWebhook, releaseDevWebhook];
-    const webhooks = [preReleaseWebhook];
+    const webhooks = isPreRelease
+      ? [preReleaseWebhook]
+      : [releaseWebhook, releaseDevWebhook];
+
+    const bodyBlocks = await markdownToBlocks(release.body);
+    const owner = repo.owner.charAt(0).toUpperCase() + repo.owner.slice(1);
+    const repositoryName =
+      repo.repo.charAt(0).toUpperCase() + repo.repo.slice(1);
+
+    const body = await Promise.all(
+      bodyBlocks.map((block) => {
+        if (block.type !== "image") {
+          return new Promise((resolve) => resolve(block));
+        }
+
+        return new Promise((resolve, reject) => {
+          cloudinary.uploader
+            .upload(block.image_url, { public_id: block.alt_text })
+            .then((result) => {
+              resolve({
+                type: "image",
+                image_url: result.url,
+                alt_text: result.public_id,
+              });
+            })
+            .catch((error) => reject(error));
+        });
+      })
+    );
+
+    const message = {
+      text: `${owner} ${repositoryName} ${release.tag_name} Released ${
+        isPreRelease ? "(candidate)" : ""
+      }!`,
+      icon_emoji: ":rocket:",
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: `${owner} ${repositoryName} ${
+              release.tag_name
+            } has been Released ${isPreRelease ? "(candidate)" : ""}! :rocket:`,
+            emoji: true,
+          },
+        },
+
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Check out the latest release notes <${release.html_url}|on Github> to see what's changed!`,
+          },
+        },
+        ...body,
+      ],
+    };
+
+    console.log("MESSAGE: ", JSON.stringify(message, null, 2));
 
     webhooks.forEach(async (webhook) => {
-      await sendReleaseNotification({
-        webhook,
-        release,
-        repo,
-        isPreRelease,
-      });
+      if (!webhook) {
+        throw new Error(
+          `No webhook URL found for ${
+            isPreRelease ? "pre-release" : "release"
+          } notification`
+        );
+      }
+
+      const slackWebhook = new IncomingWebhook(webhook);
+
+      await slackWebhook.send(message);
     });
 
     console.log("Sent notification");
