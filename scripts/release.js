640c4acc135e08bf749a2b6cef587e1a6dd2a7c8
Serhii Chupryna <schupryna@pypestream.com>
2024-02-02 01:23:02 +0100

    test

--- scripts/release.js
+++ scripts/release.js
@@ -1,30 +1,58 @@
 // https://github.com/tryfabric/notify-slack-on-release/blob/main/src/send-release-notification.ts
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
 const { markdownToBlocks } = require("@instantish/mack");
-
 const { IncomingWebhook } = require("@slack/webhook");
 const { v2 } = require("cloudinary");
 
 const cloudinary = v2;
 
-cloudinary.config({
-  cloud_name: "dvnmehqko",
-  api_key: "473343293529714",
-  api_secret: "bE-_OiM7f8DbVP9adlVgiWQx97Q",
-});
-
-// https://github.com/pypestream/frontend/assets/103273897/516dbbf3-606a-4ced-9ede-bc6ff79ce00b"
-
 const preReleaseWebhook = process.env.SLACK_PRERELEASE_WEBHOOK_URL;
 const releaseDevWebhook = process.env.SLACK_RELEASE_DEV_WEBHOOK_URL;
 const releaseWebhook = process.env.SLACK_RELEASE_WEBHOOK_URL;
 const npmToken = process.env.NPM_TOKEN;
+const cloudName = process.env.CLOUDINARY_NAME;
+const cloudinaryApiKey = process.env.CLOUDINARY_API_KEY;
+const cloudinaryApiSecret = process.env.CLOUDINARY_API_SECRET;
+
+cloudinary.config({
+  cloud_name: cloudName,
+  api_key: cloudinaryApiKey,
+  api_secret: cloudinaryApiSecret,
+});
+
+const downloadImage = async (url) => {
+  return await fetch(url, {
+    headers: {
+      Authorization: `token ${npmToken}`,
+      Accept: "application/vnd.github.v3.raw",
+    },
+  })
+    .then(async (response) => {
+      // get rid of extra url params in response
+      const { url } = response;
+      const urlObj = new URL(url);
+      urlObj.search = "";
+      const urlResult = urlObj.toString();
+
+      // grab just the filename
+      const filename = urlResult.substring(urlResult.lastIndexOf("/") + 1);
+
+      const blob = await response.blob();
+      const arrayBuffer = await blob.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      fs.writeFileSync(filename, buffer);
+
+      return filename;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 async function run() {
   try {
-    console.log(`Sending notification...`);
-
     const {
       context: {
         eventName,
@@ -37,12 +65,6 @@ async function run() {
       core.setFailed("Action should only be run on release publish events");
     }
 
-    console.log(
-      "%crelease------------------->",
-      "color: green; font-size: larger; font-weight: bold",
-      release
-    );
-
     const isPreRelease = release.prerelease;
     const webhooks = isPreRelease
       ? [preReleaseWebhook]
@@ -54,22 +76,27 @@ async function run() {
       repo.repo.charAt(0).toUpperCase() + repo.repo.slice(1);
 
     const body = await Promise.all(
-      bodyBlocks.map((block) => {
+      bodyBlocks.map(async (block) => {
         if (block.type !== "image") {
           return new Promise((resolve) => resolve(block));
         }
 
+        const filename = await downloadImage(block.image_url);
+
         return new Promise((resolve, reject) => {
           cloudinary.uploader
-            .upload(block.image_url, { public_id: block.alt_text })
+            .upload(filename, { public_id: block.alt_text })
             .then((result) => {
+              fs.unlink(filename);
               resolve({
                 type: "image",
                 image_url: result.url,
                 alt_text: result.public_id,
               });
             })
-            .catch((error) => reject(error));
+            .catch((error) => {
+              reject(error);
+            });
         });
       })
     );
@@ -102,7 +129,9 @@ async function run() {
       ],
     };
 
-    console.log("MESSAGE: ", JSON.stringify(message, null, 2));
+    // console.log("MESSAGE: ", JSON.stringify(message, null, 2));
+
+    console.log(`Sending notification...`);
 
     webhooks.forEach(async (webhook) => {
       if (!webhook) {
