4cd1f6bbbe81e37637ccd142bb3140dacb845820
Serhii Chupryna <schupryna@pypestream.com>
2024-02-08 08:19:19 +0100

    test unique names

--- scripts/slack-notification.ts
+++ scripts/slack-notification.ts
@@ -1,32 +1,9 @@
+import * as fs from "fs";
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { IncomingWebhook } from "@slack/webhook";
 import { markdownToBlocks } from "@instantish/mack";
 import { v2 as cloudinary } from "cloudinary";
-import * as fs from "fs";
-
-/**
- * TODOs & Questions:
- * - Make sure we add checks to NOT send out the release notification if release error occurs!
- * - Error handling? throwing an error could blow up the automated release if we aren't careful!
- * - Decide if every release type is treated exactly the same (ie. "loudness" based on SEMVEr)
- * - Option to have us send out a custom release notification (ex. something manually triggered)?
- * - Figure out how we handle screenshots / assets
- * - Markdown TLDR blurb?
- * - Replace hard-coded version references with dynamic value
- * - Update URLs to new `pypestream.dev` domains
- * - Update prerelease / release tasks to fire off custom Slack webhook
- * - Update script to be a function that passes in the webhook URL to use (ie. one script for both
- *   release types)
- * - In Slack App settings, confirm that the `SLACK_PRERELEASE_WEBHOOK_URL` and
- *   `SLACK_RELEASE_WEBHOOK_URL` webhook URLs (ENV vars) point to the correct channels. If not,
- *    update our Github & Vercel env vars to use the correct webhook urls.
- * - Move webhook env vars to shared vars file pulled into both release scripts
- * - Make sure the release script sends a notification out to the channel(s) we expect
- *   (ie. just the engineering channel? any others? if so, need a different env var per channel to
- *   send out notification)
- * - Do we want to call these Slack release notifications something else other than "Pypestream FE"?
- */
 
 interface Repository {
   repo: string;
@@ -78,7 +55,7 @@ interface Author {
 const preReleaseWebhook = process.env.SLACK_PRERELEASE_WEBHOOK_URL;
 const releaseDevWebhook = process.env.SLACK_RELEASE_DEV_WEBHOOK_URL;
 const releaseWebhook = process.env.SLACK_RELEASE_WEBHOOK_URL;
-const npmToken = process.env.NPM_TOKEN;
+const ghToken = process.env.NPM_TOKEN;
 const cloudName = process.env.CLOUDINARY_NAME;
 const cloudinaryApiKey = process.env.CLOUDINARY_API_KEY;
 const cloudinaryApiSecret = process.env.CLOUDINARY_API_SECRET;
@@ -92,7 +69,7 @@ cloudinary.config({
 const downloadImage = async (path: string) => {
   return await fetch(path, {
     headers: {
-      Authorization: `token ${npmToken}`,
+      Authorization: `token ${ghToken}`,
       Accept: "application/vnd.github.v3.raw",
     },
   })
@@ -137,8 +114,8 @@ async function run(): Promise<void> {
       core.setFailed("Action should only be run on release publish events");
     }
 
-    const isPreRelease = (release as Release).prerelease;
-    const webhooks = isPreRelease
+    const { prerelease } = release;
+    const webhooks = prerelease
       ? [preReleaseWebhook]
       : [releaseWebhook, releaseDevWebhook];
 
@@ -151,7 +128,7 @@ async function run(): Promise<void> {
 
     console.log("Start notification generation...");
     const body = (await Promise.all(
-      bodyBlocks.map(async (block, index) => {
+      bodyBlocks.map(async (block) => {
         if (block.type !== "image") {
           return new Promise((resolve) => resolve(block));
         }
@@ -169,7 +146,7 @@ async function run(): Promise<void> {
 
         return new Promise((resolve, reject) => {
           // upload image to cloudinary
-          console.log(`uploading image to cloudinary (${filename})...`);
+          console.log(`uploading image to cloudinary (${filename}...`);
           cloudinary.uploader
             .upload(filename, { use_filename: true, unique_filename: true })
             .then((result) => {
@@ -200,9 +177,10 @@ async function run(): Promise<void> {
       })
     )) as typeof bodyBlocks;
 
+    const bodyBlocksLimit = 50;
     const message = {
       text: `${owner} ${repositoryName} ${release.tag_name} Released ${
-        isPreRelease ? "(candidate)" : ""
+        prerelease ? "(candidate)" : ""
       }!`,
       icon_emoji: ":rocket:",
       blocks: [
@@ -212,7 +190,7 @@ async function run(): Promise<void> {
             type: "plain_text",
             text: `${owner} ${repositoryName} ${
               release.tag_name
-            } has been Released ${isPreRelease ? "(candidate)" : ""}! :rocket:`,
+            } has been Released ${prerelease ? "(candidate)" : ""}! :rocket:`,
             emoji: true,
           },
         },
@@ -225,10 +203,8 @@ async function run(): Promise<void> {
           },
         },
         ...body,
-      ],
+      ].slice(0, bodyBlocksLimit),
     };
-
-    // console.log('MESSAGE: ', JSON.stringify(message, null, 2));
 
     console.log(`Sending notification...`);
 
@@ -236,7 +212,7 @@ async function run(): Promise<void> {
       if (!webhook) {
         throw new Error(
           `No webhook URL found for ${
-            isPreRelease ? "pre-release" : "release"
+            prerelease ? "pre-release" : "release"
           } notification`
         );
       }
